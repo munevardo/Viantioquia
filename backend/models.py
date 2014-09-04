e9c6b62fdc56a7dfe7c07d2c616e7fb427ae72f1
victorm95 <victormiguel.riveramorales.1995@gmail.com>
2014-09-04 14:51:08 -0500

    Ya registra hoteles

--- backend/models.py
+++ backend/models.py
@@ -1,50 +1,50 @@
 from google.appengine.ext import ndb
 
-class BaseModel(ndb.Model):	
-	address = ndb.StringProperty(requied=False)
-	img = ndb.BlobProperty(required=False)
-	description = ndb.TextProperty(required=True)
-	price = ndb.IntegerProperty(required=False)
-	title = ndb.StringProperty(required=False)
-	ratings = ndb.FloatProperty(required=False, default=0.0)
-	town = ndb.StructuredProperty(TownModel, required=False, repeated=True)
-	
-
-class HotelModel(BaseModel):
-	name = ndb.StringProperty(requied=True, repeated=False)
-
-
-class ActivityModel(BaseModel):
-	name = ndb.StringProperty(requied=True, repeated=False)
-	start = ndb.DateProperty(required=True)
-	end = ndb.DateProperty(required=True)
-	
-	
-class RestaurantModel(BaseModel):
-	name = ndb.StringProperty(requied=True, repeated=False)
-	
-	
-class SiteModel(ndb.Model):
-	address = ndb.StringProperty(requied=False)
-	img = ndb.BlobProperty(required=False)
-	description = ndb.TextProperty(required=True)
-	title = ndb.StringProperty(required=False)
-	ratings = ndb.FloatProperty(required=False, default=0.0)
-	town = ndb.StructuredProperty(TownModel, required=False, repeated=True)
-	
-	
-class UserModel(ndb.Model):
-	name = ndb.StringProperty(requied=True)
-	img = ndb.GenericProperty(required=False)
-	username = ndb.StringProperty(required=True, repeated=False)
-	email = ndb.StringProperty(required=True)
-	password = ndb.StringProperty(required=True)
-	
-
 class DepartmentModel(ndb.Model):
 	name = ndb.StringProperty(required=True, repeated=False)
 	
 
 class TownModel(ndb.Model):
 	name = ndb.StringProperty(required=True, repeated=False)
-	department = ndb.StructuredProperty(DepartmentModel, required=True, repeated=True)
+	department = ndb.StructuredProperty(DepartmentModel, required=True)
+	
+
+class BaseModel(ndb.Model):	
+	address = ndb.StringProperty(required=False)
+	img = ndb.BlobProperty(required=False)
+	description = ndb.TextProperty(required=True)
+	price = ndb.IntegerProperty(required=False)
+	title = ndb.StringProperty(required=False)
+	ratings = ndb.FloatProperty(required=False, default=0.0)
+	town = ndb.StructuredProperty(TownModel, required=False)
+	
+
+class HotelModel(BaseModel):
+	name = ndb.StringProperty(repeated=False)
+
+
+class ActivityModel(BaseModel):
+	name = ndb.StringProperty(repeated=False)
+	start = ndb.DateProperty(required=True)
+	end = ndb.DateProperty(required=True)
+	
+	
+class RestaurantModel(BaseModel):
+	name = ndb.StringProperty(repeated=False)
+	
+	
+class SiteModel(ndb.Model):
+	address = ndb.StringProperty(required=False)
+	img = ndb.BlobProperty(required=False)
+	description = ndb.TextProperty(required=True)
+	title = ndb.StringProperty(required=False)
+	ratings = ndb.FloatProperty(required=False, default=0.0)
+	town = ndb.StructuredProperty(TownModel, required=False)
+	
+	
+class UserModel(ndb.Model):
+	name = ndb.StringProperty(required=True)
+	img = ndb.GenericProperty(required=False)
+	username = ndb.StringProperty(repeated=False)
+	email = ndb.StringProperty(required=True)
+	password = ndb.StringProperty(required=True)
--- backend/views/hotel_views.py
+++ backend/views/hotel_views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from google.appengine.ext import ndb
 from django.shortcuts import render
 from backend.forms.hotel import HotelForm
-from backend.models import HotelModel
+from backend.models import HotelModel, TownModel, DepartmentModel
 
 
 def index(request):
@@ -23,12 +23,20 @@ def new(request):
 		
 		if form.is_valid():
 		
-			hotel = HotelModel()
-			hotel.name = form.cleaned_data['name']
+			hotel = HotelModel(
+				name = form.cleaned_data['name'],
+				address = form.cleaned_data['address'],
+				description = form.cleaned_data['description'],
+				title = form.cleaned_data['title'],
+				price = form.cleaned_data['price']
+			)			
+			
+			
+			hotel.put()
 		
 			return HttpResponseRedirect('/')
 			
-		else
+		else:
 			return render(request, 'hotels/new.html', {
 				'form': form
 			})
